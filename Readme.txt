4777630896e878515fb3dd27abac09982ad82453
Esposito <info@espositoristrutturazioni.it>
2026-02-13 23:17:30 -0300

    Sitio web limpiado - versión final con solo imágenes esenciales

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Sitio Web de la empresa Renueva Hogar, empresa dedicada a la reforma de viviendas y diseño de interiores.
+Lenguajes utilizados: HTML, CSS, Java Sacript y PHP.
+Template Name: Plato
+Template URL: https://bootstrapmade.com/plato-responsive-bootstrap-website-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ footer.php
@@ -0,0 +1,81 @@
+
+<!-- FOOTER -->
+<footer id="footer" class="footer dark-background">
+    <div class="container footer-top">
+      <div class="row gy-4">
+    <!-- INDIRIZZO -->
+        <div class="col-lg-4 col-md-6 footer-about">
+          <div class="footer-contact pt-3">
+            <p>Via Montecuccoli Raimondo 8</p>
+            <p>Milano, MI</p>
+            <p class="mt-3"><strong>Telefono:</strong> <span>+39 02 1234567</span></p>
+            <p><strong>Email:</strong> <span>info@espositoristrutturazioni.it</span></p>
+          </div>
+    <!-- SOCIAL MEDIA -->
+          <div class="social-links d-flex mt-4">
+            <a href="https://x.com/"><i class="bi bi-twitter-x"></i></a>
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+            <a href="https://es.linkedin.com/"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+    <!-- MENU -->
+        <div class="col-lg-4 col-md-3 footer-links">
+          <h4>Menu</h4>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="chi-siamo.html">Chi Siamo</a></li>
+            <li><a href="servizi.html">Servizi</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contatti.html">Contatti</a></li>
+            <li><a href="politica-della-privacy.html">Politica della Privacy</a></li>
+          </ul>
+        </div>
+<!-- I NOSTRI SERVIZI FOOTER -->
+        <div class="col-lg-4 col-md-3 footer-links">
+          <h4>I Nostri Servizi</h4>
+          <ul>
+            <li ><a href="dettagli-servizi.html#servizio-pittura">Servizio di Pittura</a></li>
+            <li ><a href="dettagli-servizi.html#pavimenti-legno">Pavimenti in Legno</a></li>
+            <li ><a href="dettagli-servizi.html#idraulica">Servizio di Idraulica</a></li>
+            <li ><a href="dettagli-servizi.html#impianti-elettrici">Impianti Elettrici</a></li>
+            <li ><a href="dettagli-servizi.html#accessori-decorazione">Accessori e Decorazione</a></li>
+            <li ><a href="dettagli-servizi.html#protezione-incendi">Protezione Antincendio</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+<!-- COPYRIGHT -->
+    <div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">ESPOSITO RISTRUTTURAZIONI SRL</strong> <span>Tutti i diritti riservati</span></p>
+      <div class="credits">
+        Design by ESPOSITO RISTRUTTURAZIONI
+      </div>
+    </div>
+  </footer> <!-- FINE DI FOOTER -->
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+<!-- PAGINAS PARA EL BLOG  
+https://reformaslosangeles.es/
+https://inspirahogar.com/consejos/renovar-tu-hogar-10-ideas-creativas-para-transformar-tu-espacio/ -->
+
+</html>
